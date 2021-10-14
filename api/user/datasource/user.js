05ad5ecf2573fd72ada864ce4c6e8af6565308ca
Matheus Brehm Barni <matheusbarni@cvccorp.com.br>
2021-10-14 10:50:13 -0300

    finished some lessons

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -6,7 +6,59 @@ class UsersAPI extends RESTDataSource {
     this.baseURL = 'http://localhost:3000'
   }
   async getUsers() {
-    return this.get('/users')
+    const users = await this.get('/users')
+    return users.map(async user => ({
+      id: user.id,
+      nome: user.nome,
+      email: user.email,
+      ativo: user.ativo,
+      role: await this.get(`/roles/${user.role}`)
+    }))
+  }
+
+  async getUserById(id) {
+    const user = await this.get(`/users/${id}`)
+    return ({
+      id: user.id,
+      nome: user.nome,
+      email: user.email,
+      ativo: user.ativo,
+      role: await this.get(`/roles/${user.role}`)
+    })
+  }
+
+  async adicionaUser(user) {
+    const users = await this.getUsers()
+    const role = await this.get(`/roles?type=${user.role}`)
+    user.id = users.length + 1;
+
+    await this.post('/users', {
+      ...user,
+      role: role[0].id
+    })
+
+    return ({
+      ...user,
+      role: role[0]
+    })
+  }
+
+  async atualizaUser(novoUser) {
+    const role = await this.get(`/roles?type=${novoUser.role}`)
+    await this.put(`users/${novoUser.id}`, {
+      ...novoUser,
+      role: role[0].id
+    })
+
+    return ({
+      ...novoUser,
+      role: role[0]
+    })
+  }
+
+  async deletaUser(id) {
+    await this.delete(`users/${id}`)
+    return id
   }
 }
 
